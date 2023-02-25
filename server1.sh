7021f09376cfbf528b41c91d5336820d6cb9843c
Josh <72828296+JoshAtticus@users.noreply.github.com>
2023-02-25 15:03:11 +0800

    Create server1.sh

---
+++ server1.sh
@@ -0,0 +1,37 @@
+clear
+cd ~
+echo "Hello there! Starting Server..."
+echo "-------------------------------"
+sleep 1
+echo "Checking for any new updates..."
+cd MDWalters125
+sleep 1
+echo "Checking for MDBot updates"
+git pull
+echo "Finished checking for and downloading any new MDBot updates!"
+sleep 1
+cd ../FeedsBot
+sleep 1
+echo "Checking for FeedsBot updates"
+git pull
+echo "Finished checking for and downloading any new FeedsBot updates!"
+echo "-------------------------------"
+echo "Starting MDBot"
+sleep 1
+gnome-terminal -- bash -c "cd ../MDWalters125 && npm run start"
+echo "Started MDBot!"
+sleep 1
+echo "Starting FeedsBot"
+gnome-terminal -- bash -c "cd ../FeedsBot && npm run start"
+echo "Started FeedsBot!"
+sleep 1
+echo "Starting Minecraft Server"
+gnome-terminal -- bash -c "cd ../MCServer && ./bedrock_server"
+echo "Started Minecraft Server!"
+sleep 1
+echo "Starting Meowerimg"
+gnome-terminal -- bash -c "cd ../Meowerimg && python3 main.py"
+echo "Started Meowerimg!"
+echo "-------------------------------"
+sleep 1
+echo "Server started! Have a nice day!"
